3e14011a306b6169c02407c394a3a39508de05db
artistic8 <aadel.iimam@gmail.com>
2025-03-08 14:02:20 +0800

    ...

--- 0309/agregate.php
+++ 0309/agregate.php
@@ -1,4 +1,100 @@
 <?php
 
 return [
+	'1' => [
+		/**
+		Race 1
+		*/
+		'favorites' => '(F) 7, 10',
+		'qin(union )' => '1, 5, 7, 10',
+		'unions($10)' => '1, 5, 7, 10',
+		'union + favorites' => '1, 5, 7, 10',
+		'count union + favorites' => '4',
+	],
+	'2' => [
+		/**
+		Race 2
+		*/
+		'favorites' => '(F) 4',
+	],
+	'3' => [
+		/**
+		Race 3
+		*/
+		'favorites' => '(F) 2, 8',
+	],
+	'4' => [
+		/**
+		Race 4
+		*/
+		'favorites' => '(F) 2, 5',
+		'qin(union )' => '1, 2, 4, 5',
+		'unions($10)' => '1, 2, 4, 5',
+		'union + favorites' => '1, 2, 4, 5',
+		'count union + favorites' => '4',
+	],
+	'5' => [
+		/**
+		Race 5
+		*/
+		'favorites' => '(F) 7, 10',
+		'qin(union )' => '1, 2, 7, 10',
+		'unions($10)' => '1, 2, 7, 10',
+		'union + favorites' => '1, 2, 7, 10',
+		'count union + favorites' => '4',
+	],
+	'6' => [
+		/**
+		Race 6
+		*/
+		'favorites' => '(F) 2, 3',
+		'qin(seven )' => '2, 3, 4, 5, 6, 9, 11',
+		'sevens($10)' => '2, 3, 4, 5, 6, 9, 11',
+		'count sevens' => '7',
+		'qin(union 300)' => '2, 3, 6, 9',
+		'unions($10)' => '2, 3, 5, 6, 9, 11',
+		'union + favorites' => '2, 3, 5, 6, 9, 11',
+		'count union + favorites' => '6',
+		'inter sevens unions' => '2, 3, 5, 6, 9, 11',
+		'count inter sevens unions' => '6',
+		'qin(union 306)' => '2, 3, 6, 11',
+		'qin(union 309)' => '2, 3, 9, 11',
+		'qin(union 402)' => '2, 3, 5, 6',
+		'qin(union 405)' => '2, 3, 5, 9',
+		'qin(union 597)' => '2, 3, 5, 11',
+	],
+	'7' => [
+		/**
+		Race 7
+		*/
+		'favorites' => '(F) 7',
+	],
+	'8' => [
+		/**
+		Race 8
+		*/
+		'favorites' => '(F) 3, 6',
+	],
+	'9' => [
+		/**
+		Race 9
+		*/
+		'favorites' => '(F) 1, 11',
+		'qin(union )' => '1, 2, 3, 11',
+		'unions($10)' => '1, 2, 3, 11',
+		'union + favorites' => '1, 2, 3, 11',
+		'count union + favorites' => '4',
+	],
+	'10' => [
+		/**
+		Race 10
+		*/
+		'favorites' => '(F) 1',
+	],
+	'11' => [
+		/**
+		Race 11
+		*/
+		'favorites' => '(F) 4, 9',
+	],
 ];
