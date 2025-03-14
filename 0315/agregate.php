062be8c9f6d0afbc75edadfc82c207dfe395ef4a
artistic8 <aadel.iimam@gmail.com>
2025-03-14 14:23:37 +0800

    ...

--- 0315/agregate.php
+++ 0315/agregate.php
@@ -5,9 +5,14 @@ return [
 		/**
 		Race 1
 		*/
-		'favorites' => '(F) 2, 7, 9, 10, 12',
+		'favorites' => '(F) 2, 3, 7, 9, 10, 12',
+		'qin(seven )' => '1, 2, 3, 7, 9, 10, 12',
 		'unions($10)' => '1, 7, 9, 12',
-		'union + favorites' => '1, 2, 7, 9, 10, 12',
-		'count union + favorites' => '6',
+		'union + favorites' => '1, 2, 3, 7, 9, 10, 12',
+		'count union + favorites' => '7',
+		'sevens($10)' => '1, 2, 3, 7, 9, 10, 12',
+		'count sevens' => '7',
+		'inter sevens unions' => '1, 7, 9, 12',
+		'count inter sevens unions' => '4',
 	],
 ];
--- 0315/bets179.php
+++ 0315/bets179.php
@@ -5,12 +5,13 @@ return [
 		/**
 		Race 1
 		*/
-		'favorites' => '2, 7, 9, 10, 12',
+		'favorites' => '2, 3, 7, 9, 10, 12',
 		'runners' => '1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12',
 		'win inter' => '1',
-		'qin inter' => '1, 7',
-		'trio inter' => '1, 2, 6, 7',
+		'qin inter' => '1',
+		'trio inter' => '1, 2, 6',
 		'bets' => [
+			'qin(seven 179)' => '1, 2, 3, 7, 9, 10, 12',
 		],
 		'total bets' => 0,
 	],
