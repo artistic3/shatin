216469602f971b53476732aa72803b4ef61829ee
artistic8 <aadel.iimam@gmail.com>
2025-03-14 13:13:16 +0800

    ...

--- 0315/agregate.php
+++ 0315/agregate.php
@@ -1,13 +1,4 @@
 <?php
 
 return [
-	'1' => [
-		/**
-		Race 1
-		*/
-		'favorites' => '(F) 2, 7, 9, 10, 12',
-		'unions($10)' => '1, 7, 9, 12',
-		'union + favorites' => '1, 2, 7, 9, 10, 12',
-		'count union + favorites' => '6',
-	],
 ];
--- 0315/bets533.php
+++ 0315/bets533.php
@@ -1,19 +1,6 @@
 <?php
 
 return [
-	'1' => [
-		/**
-		Race 1
-		*/
-		'favorites' => '2, 7, 9, 10, 12',
-		'runners' => '1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12',
-		'win inter' => '',
-		'qin inter' => '',
-		'trio inter' => '2, 6',
-		'bets' => [
-		],
-		'total bets' => 0,
-	],
 ];
 //total qin: 0
 //total: 0
