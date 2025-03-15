fd7dbee8c87253f552f6d2a901f267a5327a13f4
artistic8 <aadel.iimam@gmail.com>
2025-03-15 13:22:14 +0800

    ...

---
+++ 20250315/bets.php
@@ -0,0 +1,29 @@
+<?php
+
+return [
+	'1' => [
+		/**
+		Race 1
+		*/
+		'favorites' => '2, 3, 7, 9, 10, 12',
+		'runners' => '1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12',
+		'official win' => '5, 2, 10, 3',
+		'win amount' => 72.5,
+		'qin amount' => 179.5,
+		'trio amount' => 358,
+		'place amount' => [
+			5 => 22.5,
+			2 => 16.5,
+			10 => 21,
+		],
+		'win inter' => '1',
+		'qin inter' => '1, 10',
+		'trio inter' => '1, 2, 6, 10',
+		'bets' => [
+			'qin(seven )' => '1, 2, 3, 7, 9, 10, 12',
+		],
+		'total bets' => 0,
+	],
+];
+//total qin: 0
+//total: 0
