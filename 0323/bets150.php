a69e2f5d8c6ee77d375935bbade5c6986ccc4fc5
artistic8 <aadel.iimam@gmail.com>
2025-03-22 13:02:37 +0800

    ...

---
+++ 0323/bets150.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+	'1' => [
+		/**
+		Race 1
+		*/
+		'favorites' => '4, 5, 7',
+		'runners' => '1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14',
+		'win inter' => '',
+		'qin inter' => '1, 2, 9',
+		'trio inter' => '1, 2, 4, 5, 7, 9, 11',
+		'bets' => [
+		],
+		'total bets' => 0,
+	],
+];
+//total qin: 0
+//total: 0
---
+++ 0323/bets567.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+	'1' => [
+		/**
+		Race 1
+		*/
+		'favorites' => '4, 5, 7',
+		'runners' => '1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14',
+		'win inter' => '5',
+		'qin inter' => '2, 5, 9',
+		'trio inter' => '2, 4, 5, 7, 9, 11',
+		'bets' => [
+		],
+		'total bets' => 0,
+	],
+];
+//total qin: 0
+//total: 0
